0a2083f116f45b1f1d663984108628cfe71bdb2f
luongtrongdat <luongdat114zu@gmail.com>
2025-03-03 22:24:00 +0700

    nop btvn

--- Session19/baitap/baitap02.js
+++ Session19/baitap/baitap02.js
@@ -1,6 +1,11 @@
-let input = prompt("Nhập một ký tự:");
-if (input.indexOf(0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) != -1) {
-    console.log("không phải chữ cái");
-} else {
+let input = prompt("Nhập một ký tự: ");
+let chucai = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+if (input.length === 1 && chucai.indexOf(input) !== -1) {
     console.log(`ký tự ${input} là chữ cái`);
+    document.write(`ký tự ${input} là chữ cái`);
+    alert(`ký tự ${input} là chữ cái`);
+} else {
+    console.log("không phải chữ cái");
+    document.write("không phải chữ cái");
+    alert("không phải chữ cái");
 }
--- Session19/baitap/baitap04.js
+++ Session19/baitap/baitap04.js
@@ -1,7 +1,5 @@
 let number = prompt("Nhập một số:");
-if (!Number.isInteger(number)) {
-    console.log("Đây không phải số");
-} else if (number % 3 === 0 && number % 5 === 0) {
+if (number % 3 === 0 && number % 5 === 0) {
     console.log(number + " chia hết cho cả 3 và 5");
 } else {
     console.log(number + " không chia hết cho cả 3 và 5");
